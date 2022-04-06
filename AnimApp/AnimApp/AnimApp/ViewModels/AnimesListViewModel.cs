e97d785ecb2306e556bc74ccec2e11e357657105
MylèneG06 <mylene.alliez@gmail.com>
2022-04-06 11:49:11 +0200

    base onglets animes/mangas

---
+++ AnimApp/AnimApp/AnimApp/ViewModels/AnimesListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimApp.ViewModels
+{
+    class AnimesListViewModel : BaseViewModel
+    {
+    }
+}
---
+++ AnimApp/AnimApp/AnimApp/ViewModels/TestViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimApp.ViewModels
+{
+    class TestViewModel : BaseViewModel
+    {
+        public TestViewModel()
+        {
+            Title = "AniMangApp";
+            //OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+        }
+
+        //public ICommand OpenWebCommand { get; }
+    }
+}
