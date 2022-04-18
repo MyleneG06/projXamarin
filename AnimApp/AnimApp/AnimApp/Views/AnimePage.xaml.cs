07fa648eb333caaf85d6a12d62bed26732550ee6
Kevin_Pouzaud <kepouz08@gmail.com>
2022-04-18 18:06:09 +0200

    Fonctionnalité likes + image like
    - Ajout de la fonctionnalité de likes, données persistantes comme les vues
    - Ajout d'un image clickable (like) et modification de l'interface
    - Ajout du code pour la persistance des likes et fonction clickable

--- AnimApp/AnimApp/AnimApp/Views/AnimePage.xaml.cs
+++ AnimApp/AnimApp/AnimApp/Views/AnimePage.xaml.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -17,6 +17,35 @@ namespace AnimApp.Views
         {
             InitializeComponent();
             BindingContext = new AnimeViewModel(animeSelected);
+            prefNameViews += animeTitle.Text;
+            prefNameLikes += animeTitle.Text;
+            nbViews = Preferences.Get(prefNameViews, 0);
+            nbLikes = Preferences.Get(prefNameLikes, 0);
+            likes.Text = nbLikes.ToString();
+        }
+
+        int nbViews = 0;
+        int nbLikes = 0;
+        string prefNameViews = "nbViewsAnime";
+        string prefNameLikes = "nbLikesAnime";
+
+        void Click_To_Like(object sender, EventArgs e)
+        {
+            nbLikes++;
+            likes.Text = nbLikes.ToString();
+            Preferences.Set(prefNameLikes, nbLikes);
+        }
+
+        protected override void OnAppearing()
+        {
+            nbViews++;
+            views.Text = nbViews.ToString();
+            Preferences.Set(prefNameViews, nbViews);
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
         }
     }
 }
