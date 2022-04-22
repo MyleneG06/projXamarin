7b6cdd5226bfc48d19dcffc574e13a7d9d8fdbc6
MylèneG06 <mylene.alliez@gmail.com>
2022-04-22 11:50:51 +0200

    Toast translation on Droi/Ios for animés

---
+++ AnimApp/AnimApp/AnimApp.Android/Services/ToastTranslateDroid.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AnimApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AnimApp.Droid.Services.ToastTranslateDroid))]
+
+namespace AnimApp.Droid.Services
+{
+    class ToastTranslateDroid : IToastTranslateService
+    {
+        public void DisplayTranslate(string message)
+        {
+            Toast.MakeText(Android.App.Application.Context, message, ToastLength.Long).Show();
+        }
+    }
+}
---
+++ AnimApp/AnimApp/AnimApp.iOS/Services/ToastTranslateIOS.cs
@@ -0,0 +1,47 @@
+﻿using AnimApp.Services;
+using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AnimApp.iOS.Services.ToastTranslateIOS))]
+
+namespace AnimApp.iOS.Services
+{
+    public class ToastTranslateIOS : IToastTranslateService
+    {
+        const double DELAY = 3;
+
+        NSTimer alertDelay;
+        UIAlertController alert;
+        public void DisplayTranslate(string message)
+        {
+            ShowAlert(message, DELAY);
+        }
+
+        void ShowAlert(string message, double seconds)
+        {
+            alertDelay = NSTimer.CreateScheduledTimer(seconds, (obj) =>
+            {
+                dismissMessage();
+            });
+            alert = UIAlertController.Create(null, message, UIAlertControllerStyle.Alert);
+            UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewController(alert, true, null);
+        }
+
+        void dismissMessage()
+        {
+            if (alert != null)
+            {
+                alert.DismissViewController(true, null);
+            }
+            if (alertDelay != null)
+            {
+                alertDelay.Dispose();
+            }
+        }
+    }
+}
